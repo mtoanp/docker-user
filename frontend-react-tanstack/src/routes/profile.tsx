f17475e4991b8b6f89af267642d6b39fcbcd4790
nyukeit <nyukeit@outlook.com>
2024-09-06 12:39:22 +0200

    fix: relaunch basic react app with vite

--- frontend-react-tanstack/src/routes/profile.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Profile } from "../pages";
-
-export const Route = createFileRoute("/profile")({
-  component: Profile,
-});
--- frontend-react-tanstack/src/routes/users/$userId.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { UserDetails } from "../../pages";
-
-export const Route = createFileRoute("/users/$userId")({
-  component: UserDetails,
-});
